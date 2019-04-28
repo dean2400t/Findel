24332ecafc337e33ae6c1bf4058e90ee8db2b7c6
dean2400t <dean2400t@gmail.com>
2019-04-28 15:18:58 +0300

    adding security with config and env.variables

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,13 @@
 const winston=require("winston");
 const mongoose=require("mongoose");
+const config = require('config');
 
 //"mongodb://dean2400t:Dm135135@ds147946.mlab.com:47946/heroku_b3ckh1g4"
 //'mongodb://localhost/findel'
 module.exports= function()
 {
-    mongoose.connect('mongodb://dean2400t:Dm135135@ds147946.mlab.com:47946/heroku_b3ckh1g4')
+    const db_connection=config.get('db_connection');
+    mongoose.connect(db_connection)
     .then (()=> console.log("connected to mongodb"))
     
 }
