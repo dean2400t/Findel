d0e0da29cf6775f5bc3665ab188b236c4c899356
dean2400t <dean2400t@gmail.com>
2019-04-28 13:46:10 +0300

    added db connection

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 const winston=require("winston");
 const mongoose=require("mongoose");
 
+//"mongodb://dean2400t:Dm135135@ds147946.mlab.com:47946/heroku_b3ckh1g4"
+//'mongodb://localhost/findel'
 module.exports= function()
 {
-    mongoose.connect('mongodb://localhost/findel')
+    mongoose.connect('mongodb://dean2400t:Dm135135@ds147946.mlab.com:47946/heroku_b3ckh1g4')
     .then (()=> console.log("connected to mongodb"))
     
 }
