a7c7ac340c10653bcaf61814bf75197ca8822d2a
dean2400t <dean2400t@gmail.com>
2019-04-28 14:13:45 +0300

    changed index.js to index.html

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     //
     app.get('*', (req, res) => {
-      res.sendfile(path.join(__dirname = 'client/build/index.js'));
+      res.sendfile(path.join(__dirname = 'client/build/index.html'));
     })
   }
 
