5517914a1d9020d7bb60153c9fc894336dd7e304
dean2400t <dean2400t@gmail.com>
2019-05-26 00:46:33 +0300

    alpha 1.1.0

--- client/build/precache-manifest.27080807f11c13086c5bd7ae9316ca60.js
+++ client/build/precache-manifest.27080807f11c13086c5bd7ae9316ca60.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "88eb46ad9f26b0c9c3bc",
+    "revision": "711e7c448742b7c8536b",
     "url": "/static/css/main.04c02887.chunk.css"
   },
   {
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/static/js/2.16eb7457.chunk.js"
   },
   {
-    "revision": "88eb46ad9f26b0c9c3bc",
-    "url": "/static/js/main.73314a52.chunk.js"
+    "revision": "711e7c448742b7c8536b",
+    "url": "/static/js/main.988dba5a.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "4c25a8aa7741d09d7988",
     "url": "/static/css/2.f15d3845.chunk.css"
   },
   {
-    "revision": "7c2bac76716749d8cb8df7f7971ff438",
+    "revision": "3fdbe00ee0159c1acfc6b9946d299fb3",
     "url": "/index.html"
   }
 ];
